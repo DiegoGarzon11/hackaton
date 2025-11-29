2cf0d23c0208160b299c0417a45dec9eae82fa84
sebastian <sebasga12345@gmail.com>
2025-11-29 00:35:32 -0500

    v1

--- front/src/components/Chat.tsx
+++ front/src/components/Chat.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import '../App.css';
 import { ArrowLeft, Minimize, SendHorizontal, UserStar } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 interface Route {
 	id: number;
@@ -113,9 +114,8 @@ function Chat() {
 			<div className='h-full   '>
 				<div
 					id='padre'
-					className={` bg-linear-to-r from-blue-900/80 to-violet-500/70 w-sm ${
-						closeChat ? 'h-10 overflow-hidden' : 'h-1/5'
-					} pb-2 mb-3 rounded-md  `}>
+					className={` bg-linear-to-r from-blue-900/80 to-violet-500/70 w-sm ${closeChat ? 'h-10 overflow-hidden' : 'h-1/5'
+						} pb-2 mb-3 rounded-md  `}>
 					<div className='flex justify-between items-center '>
 						<button
 							onClick={() => {
@@ -194,6 +194,8 @@ function Chat() {
 										) : (
 											''
 										)}
+
+
 
 										{option && (
 											<>
